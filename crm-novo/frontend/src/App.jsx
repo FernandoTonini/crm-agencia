fa0892ca432b3812edc7655669d8472e55f44082
FernandoTonini <fernandotonini444@gmail.com>
2025-10-27 13:10:20 -0300

    Update App.jsx

--- crm-novo/frontend/src/App.jsx
+++ crm-novo/frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Register } from './pages/Register';
 import { Dashboard } from './pages/Dashboard';
 import { Leads } from './pages/Leads';
 import { Contracts } from './pages/Contracts';
-import Services from './pages/Services';
+import { Services } from './pages/Services';
 import { Loading } from './components/Loading';
 
 // Componente para rotas protegidas
